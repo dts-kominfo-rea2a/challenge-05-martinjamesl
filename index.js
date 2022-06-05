f2d789ef53b61f40c5859ccdaac7de4c15e0e846
martinjamesl <mjl.drst@gmail.com>
2022-06-05 23:44:47 +0700

    feat: challenge-05-martinjamesl commit

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (listNama, cbSort) => {
+  sortNama = cbSort(listNama);
+  let arrayNama = [];
+  for (let counter = 0; counter < sortNama.length; counter++) {
+    arrayNama.push(`${counter + 1}. ${sortNama[counter]}`);
+  }
+  return arrayNama;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  let data = arr.sort();
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  let data = arr.sort().reverse();
+  return data;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
